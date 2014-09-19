f89334459a478e0da97ee0eefba4ee5699cafd62
Marco Rougeth <marco@rougeth.com>
2014-09-19 12:37:46 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ccrypt',
+    version='0.10',
+    description='Implementation of classical encryption algorithms',
+    author='Marco Rougeth',
+    author_email='marco@rougeth.com',
+    url='https://github.com/rougeth/ccrypt',
+    license='',
+)
