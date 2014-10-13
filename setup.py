72d40556a645a683708719e3d1e209ebe4072ea8
Marco Rougeth <marco@rougeth.com>
2014-10-13 11:20:49 -0300

    Updated Travis config to support coveralls

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
     name='ccrypt',
-    version='0.10',
+    version='0.02',
     description='Implementation of classical encryption algorithms',
     author='Marco Rougeth',
     author_email='marco@rougeth.com',
     url='https://github.com/rougeth/ccrypt',
     license='',
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
