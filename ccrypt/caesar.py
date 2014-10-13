a9e218339043d97249e0e54ae2a1c86b0d556c7c
Marco Rougeth <marco@rougeth.com>
2014-10-13 10:26:07 -0300

    Updated Caesar with simple substitution class

--- ccrypt/caesar.py
+++ ccrypt/caesar.py
@@ -1,14 +1,8 @@
+from ccrypt.simple_substitution import SimpleSubstitution
 from ccrypt.utils import k27 as alphabet
 
 
-def shift(char):
-    index = alphabet.index(char)
-    index = (index + 3)  % 27
-    return alphabet[index]
+class Caesar(SimpleSubstitution):
 
-def caesar(message):
-    cryptogram = ''
-    for char in message:
-        cryptogram = cryptogram + shift(char)
-
-    return cryptogram
+    def __init__(self):
+        super().__init__(alphabet, 3)
