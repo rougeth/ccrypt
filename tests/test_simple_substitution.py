a630bdd20fc9215b8fa687bf360a4da5bf2a4d1f
Marco Rougeth <marco@rougeth.com>
2014-10-13 12:08:07 -0300

    Added test to check the key when it is bigger than the alphabet in simple substitution algorithm

--- tests/test_simple_substitution.py
+++ tests/test_simple_substitution.py
@@ -9,20 +9,25 @@ params = {
     'key': 3
 }
 
-def test_ss_params():
+
+def test_params():
+    s = SimpleSubstitution(k27, 28)
+    assert s.key == 1
+
+def test_key_bigger_than_alphabet():
     s = SimpleSubstitution(**params)
     assert s.alphabet == params['alphabet']
     assert s.key == params['key']
 
-def test_ss_encrypt_shift():
+def test_encrypt_shift():
     s = SimpleSubstitution(**params)
     assert s.encrypt_shift('a') == 'd'
 
-def test_ss_decrypt_shift():
+def test_decrypt_shift():
     s = SimpleSubstitution(**params)
     assert s.decrypt_shift('d') == 'a'
 
-def test_ss_encrypt_message():
+def test_encrypt_message():
     s = SimpleSubstitution(**params)
     cryptogram = s.encrypt('Hello World')
     assert cryptogram == 'khoorczruog'
